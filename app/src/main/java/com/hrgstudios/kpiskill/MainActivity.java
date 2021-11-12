fc687bb6bbf1768443d24f35c9ce0d89f5da66c6
HeroRickyGAMES <ricojn9@gmail.com>
2021-11-12 10:55:09 -0300

    Add o JSON do app para ter acesso ao banco de dados em tempo real

--- app/src/main/java/com/hrgstudios/kpiskill/MainActivity.java
+++ app/src/main/java/com/hrgstudios/kpiskill/MainActivity.java
@@ -7,11 +7,19 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText textNome, textCPF, textIdade, textSenha, editTextSenha;
+    EditText textNome, textCPF, textIdade, textSenha, textEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Atribuir os ids com a interface gráfica do aplicativo
+        textNome = findViewById(R.id.textNome);
+        textCPF = findViewById(R.id.textCPF);
+        textIdade = findViewById(R.id.textIdade);
+        textEmail = findViewById(R.id.textEmail);
+        textSenha = findViewById(R.id.textSenha);
     }
 }
