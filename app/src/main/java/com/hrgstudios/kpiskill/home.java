6b4268d53400be9969fddb91250eab7fed14b130
HeroRickyGAMES <ricojn9@gmail.com>
2021-11-12 13:09:52 -0300

    Começando a brincadeira kkk

--- app/src/main/java/com/hrgstudios/kpiskill/home.java
+++ app/src/main/java/com/hrgstudios/kpiskill/home.java
@@ -1,8 +1,13 @@
 package com.hrgstudios.kpiskill;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Dias", Dias.class)
+                //.add(R.string.titleB, PageFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
     }
 }
