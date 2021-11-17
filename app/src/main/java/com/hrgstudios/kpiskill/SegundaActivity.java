98020e6ff6ce0edd366d147162a34c56fe0def13
HeroRickyGAMES <ricojn9@gmail.com>
2021-11-17 12:28:40 -0300

    Algumas alterações com a SegundaActivity

--- app/src/main/java/com/hrgstudios/kpiskill/SegundaActivity.java
+++ app/src/main/java/com/hrgstudios/kpiskill/SegundaActivity.java
@@ -7,10 +7,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SegundaActivity extends AppCompatActivity {
 
     TextView Cliente01;
     EditText AddClienteEdText;
+    public DatabaseReference referencia = FirebaseDatabase.getInstance().getReference("funcionarios");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,30 @@ public class SegundaActivity extends AppCompatActivity {
     }
 
     public void Add(View view){
+
+        //String[] Pessoas = {"Ricky", "Leandro"};
+
+        FirebaseUser usuarioLogado = FirebaseAuth.getInstance().getCurrentUser();
+
+        String getUID = usuarioLogado.getUid();
+
         String cliente1 = AddClienteEdText.getText().toString();
 
+        String Cliente1 = "Cliente";
+        int n = 1;
+
+        referencia.child(getUID).child("dias").child("SegundaFeira").child(Cliente1 + 1).setValue(cliente1);
+
+        if(n == 1){
+            System.out.println("O valor foi adicionado...");
+
+            String Cliente2 = "Cliente";
+
+            referencia.child(getUID).child("dias").child("SegundaFeira").child(Cliente2 + n + n).setValue(Cliente2);
+
+        }
+
+        System.out.println(getUID + cliente1);
         Cliente01.setText(cliente1);
     }
 }
